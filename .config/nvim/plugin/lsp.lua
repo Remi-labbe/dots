ad9a8ef7416cf15db372b0e48052bfb63f79549e
Remi Labbe <remi.labbe@docaposte.fr>
2025-12-24 13:10:43 +0100

    treesitter + custom blink

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,30 +1,40 @@
 vim.lsp.config(
-  "*",
-  ---@type vim.lsp.Config
-  {
-    capabilities = require("blink.cmp").get_lsp_capabilities(),
-  }
+	"*",
+	---@type vim.lsp.Config
+	{
+		capabilities = require("blink.cmp").get_lsp_capabilities(),
+	}
 )
 
 vim.api.nvim_create_autocmd('LspAttach', {
-			group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-			callback = function(ev)
-				-- Enable completion triggered by <c-x><c-o>
-				vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local opts = { buffer = ev.buf }
-        -- local telescopeBuiltins = require("telescope.builtin")
-				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-				vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-				-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-				-- vim.keymap.set('n', 'gr', telescopeBuiltins.lsp_references, opts)
-				vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-				vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-				vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
-			end,
+		vim.diagnostic.config({
+			virtual_lines = {
+				current_line = true,
+			},
 		})
 
-vim.lsp.enable({ 'lua_ls', 'gopls', 'ts_ls' })
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		-- local telescopeBuiltins = require("telescope.builtin")
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		-- vim.keymap.set('n', 'gr', telescopeBuiltins.lsp_references, opts)
+		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+	end,
+})
+
+vim.lsp.enable({
+	-- 'lua_ls',
+	-- 'gopls',
+	'ts_ls'
+})
